5af1724b37bcdacf0730a1ba304155e2c8134ba1
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-21 15:14:43 -0300

    fit/teste

---
+++ ex009/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tags</title>
+</head>
+<body>
+    <?php 
+    $nome = "fabiano";
+    $sobrenome = "Hermann";
+    echo "Qual é o seu nome completo $nome $sobrenome ?"
+    ?>
+</body>
+</html>
