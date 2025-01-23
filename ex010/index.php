2a49714e2dadd7cb65faa96cbed72881b69fe4a6
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-22 23:31:05 -0300

    fit/emoji

--- ex010/index.php
+++ ex010/index.php
@@ -6,14 +6,15 @@
     <title>Strings</title>
 </head>
 <body>
-    <h1>Teste do apache</h1>
+<h1>Teste do apache</h1>
     <?php 
     //$nome = "Fabiano"; 
     //$sobrenome = "Hermann";
     //echo "Qual seu nome $nome $sobrenome?";
     //echo 'Qual seu nome $nome $sobrenome?';
 
-    const Canal = "Curso em Vídeo\u{1F99}";
+    const CANAL = "Curso em Vídeo \u{1F499}";
+    echo "Eu adoro o " . CANAL;
     
     
     ?>
