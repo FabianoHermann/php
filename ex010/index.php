2ac6486c49383f22684f47e2dc024706e73e2d0e
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-22 23:14:31 -0300

    fit/up

---
+++ ex010/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <h1>Teste do apache</h1>
+    <?php 
+    //$nome = "Fabiano"; 
+    //$sobrenome = "Hermann";
+    //echo "Qual seu nome $nome $sobrenome?";
+    //echo 'Qual seu nome $nome $sobrenome?';
+
+    const Canal = "Curso em Vídeo\u{1F99}";
+    
+    
+    ?>
+</body>
+</html>
