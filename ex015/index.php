3e5e9dd8b5796478372621c0ee1925b373d6aab1
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-24 20:48:54 -0300

    up

--- ex015/index.php
+++ ex015/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Expressões Aritméticas</title>
 </head>
 <body>
+    <?php 
+    $res = 50 / (2 + 3) **2;
+    echo "O Resultado é $res";
+    ?>
     
 </body>
 </html>
