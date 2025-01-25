8e7f19d64e734aae8569b075568910e540837b44
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-24 21:10:45 -0300

    up

--- ex015/index.php
+++ ex015/index.php
@@ -7,9 +7,25 @@
 </head>
 <body>
     <?php 
-    $res = 50 / (2 + 3) **2;
-    echo "O Resultado é $res";
+    //$res = 50 / (2 + 3) **2;
+    //echo "O Resultado é $res";
+
+    //$r = base_convert(1011,2,10);
+    //print ("A resposta é $r");
+
+    //$r = intdiv(5,2);
+    //Echo "O resultado da divisão inteira é $r"
+
+    //$r= min(90,45,89,54,52,47,65,24,57,62);
+    //echo "Qual é o valor minímo entre esses números $r";
+
+    //$r= max(90,45,89,54,52,47,65,24,57,62);
+    //echo "Qual é o valor minímo entre esses números $r";
+
+    $r = M_PI ;
+    echo "Qual o valor do número PI ? <p> Resposta:$r<p/>"
     ?>
+
     
 </body>
 </html>
