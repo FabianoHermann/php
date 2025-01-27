19de6487a0db66c9d1b07ddaf2500d15f6981934
Fabiano <fabiano29hermanngba@gmail.com>
2025-01-27 14:39:49 -0300

    feat/up

--- ex019/moeda.php
+++ ex019/moeda.php
@@ -10,14 +10,13 @@
     <h1>Resultado de Conversão</h1>
 
     <?php 
-    $início = date("m-d-Y", strtotime("-7 days"));
+    $inicio = date("m-d-Y", strtotime("-7 days"));
     $fim = date("m-d-Y");
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'\&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
     $dados = json_decode(file_get_contents($url), true);
 
-    $cotação = $dados["value"][0]["cotacaoCompra"];
-    echo "A cotação foi $cotação";
+    $cotação =  $dados["value"][0]["cotacaoCompra"];
     $real = $_REQUEST ["real"] ?? 0;
     $dolar = $real / $cotação;
 
